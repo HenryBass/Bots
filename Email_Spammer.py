992c5231b22e0a03ef2bbd47d5e213bfebe5f406
HenryBass <63601449+HenryBass@users.noreply.github.com>
2020-10-23 15:57:32 -0400

    Verifies both names now

--- Email_Spammer.py
+++ Email_Spammer.py
@@ -1,21 +1,29 @@
 import smtplib
 from email.message import EmailMessage
 import re
+import time
 
 print("Welcome to EmailSpammer! Login to your Email:")
-EmailAdd = input("Email: ")
-Pass = input("Password: ")
 
-#Regular expression to check email validity.
+# Regular expression to check email validity.
 regex = '^[a-zA-Z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
 x = 1
-emailTo = [] #List to store emails
-getAgain = True 
+emailTo = []  # List to store emails
+getAgain = True
+while getAgain is True:
+    EmailAdd = input("Email: ")
+    if re.search(regex, EmailAdd):
+        getAgain = False
+    else:
+        print("Invalid Email. Please try again.")
+        getAgain = True
 
-#Check email for validity and get multiple emails
+Pass = input("Password: ")
+getAgain = True
+# Check email for validity and get multiple emails
 while getAgain is True:
     getEmail = input("To: ")
-    if(re.search(regex, getEmail)):
+    if (re.search(regex, getEmail)):
         emailTo.append(getEmail)
         getAgain = input("Do you want to add another email? Y/N ")
         if getAgain.lower() == "y":
@@ -34,7 +42,10 @@ msg.set_content(input("Content: "))
 y = input("Number of times to send Email: ")
 while x <= int(y):
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(EmailAdd, Pass)
-        smtp.send_message(msg)
+        try:
+            smtp.login(EmailAdd, Pass)
+            smtp.send_message(msg)
+        except:
+            print("Sign in was blocked or Invalid Account. Make sure that you have ''Less secure apps'' enabled on your email.")
     print("Email Sent.")
-    x = x + 1
+    x += 1
